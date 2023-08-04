8f69d3bc36a9116f8a7b2b45959be2f07ce0dad6
maurommacias <141372985+maurommacias@users.noreply.github.com>
2023-08-04 09:35:36 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,98 @@
+const form = document.querySelector("#credit-card");
+
+const cardNumber = document.querySelector("#card-number");
+const cardHolder = document.querySelector("#name-text");
+const cardExpiration = document.querySelector("#valid-thru-text");
+const cardCVV = document.querySelector("#cvv-text");
+
+const cardNumberText = document.querySelector(".number-vl");
+const cardHolderText = document.querySelector(".name-vl");
+const cardExpirationText = document.querySelector(".expiration-vl");
+const cardCVVText = document.querySelector(".cvv-vl");
+
+// Función para validar el número de tarjeta utilizando el algoritmo de Luhn
+function isValidLuhn(cardNumber) {
+    let sum = 0;
+    let numDigits = cardNumber.length;
+    let parity = numDigits % 2;
+
+    for (let i = 0; i < numDigits; i++) {
+        let digit = parseInt(cardNumber[i]);
+
+        if (i % 2 === parity) {
+            digit *= 2;
+            if (digit > 9) {
+                digit -= 9;
+            }
+        }
+
+        sum += digit;
+    }
+
+    return sum % 10 === 0;
+}
+
+cardNumber.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardNumberText.innerText = "1234 5678 9101 1121";
+    } else {
+        const valuesOfInput = e.target.value.replaceAll(" ", "");
+
+        if (e.target.value.length > 14) {
+            e.target.value = valuesOfInput.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+            cardNumberText.innerHTML = valuesOfInput.replace(/(\d{4})(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3 $4");
+        } else if (e.target.value.length > 9) {
+            e.target.value = valuesOfInput.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
+            cardNumberText.innerHTML = valuesOfInput.replace(/(\d{4})(\d{4})(\d{0,4})/, "$1 $2 $3");
+        } else if (e.target.value.length > 4) {
+            e.target.value = valuesOfInput.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+            cardNumberText.innerHTML = valuesOfInput.replace(/(\d{4})(\d{0,4})/, "$1 $2");
+        } else {
+            cardNumberText.innerHTML = valuesOfInput
+        }
+    }
+});
+
+cardHolder.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardHolderText.innerHTML = "Leonel Ronaldo";
+    } else {
+        cardHolderText.innerHTML = e.target.value.toUpperCase();
+    }
+});
+
+cardExpiration.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardExpirationText.innerHTML = "02/40";
+    } else {
+        const valuesOfInput = e.target.value.replace("/", "");
+
+        if (e.target.value.length > 2) {
+            e.target.value = valuesOfInput.replace(/^(\d{2})(\d{0,2})/, "$1/$2");
+            cardExpirationText.innerHTML = valuesOfInput.replace(/^(\d{2})(\d{0,2})/, "$1/$2");
+        } else {
+            cardExpirationText.innerHTML = valuesOfInput;
+        }
+    }
+});
+
+cardCVV.addEventListener("keyup", (e) => {
+    if (!e.target.value) {
+        cardCVVText.innerHTML = "123";
+    } else {
+        cardCVVText.innerHTML = e.target.value;
+    }
+});
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    // Verificar si el número de tarjeta es válido antes de enviar el formulario
+    const cardNumberWithoutSpaces = cardNumber.value.replaceAll(" ", "");
+    if (!isValidLuhn(cardNumberWithoutSpaces)) {
+        alert("Tarjeta inválida. Por favor, verifica el número.");
+        return;
+    }
+
+    alert("Su Tarjeta es válida. Felicidades :)");
+});
